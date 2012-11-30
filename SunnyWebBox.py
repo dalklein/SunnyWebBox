21ba73da1af65d7130c632634945aa92db6aef94
Joerg Raedler <joerg@j-raedler.de>
2012-11-30 10:24:38 +0100

    implemented UDP Stream communication, compatibility with python 3

--- SunnyWebBox.py
+++ SunnyWebBox.py
@@ -18,7 +18,22 @@
 # http://files.sma.de/dl/2585/SWebBoxRPC-BEN092713.pdf
 #
 
-import json, httplib, hashlib
+import sys, json, hashlib
+
+# handle python 2 and 3
+if sys.version_info >= (3, 0):
+    py3 = True
+    def str2buf(s):
+        return bytes(s, 'latin1')
+    def buf2str(b):
+        return str(b, 'latin1')
+else:
+    py3 = False
+    def str2buf(s):
+        return bytes(s)
+    def buf2str(b):
+        return unicode(b, 'latin1')
+
 
 class Counter:
     """generate increasing numbers with every call - just for convenience"""
@@ -32,8 +47,11 @@ class Counter:
         return i
 
 
-class SunnyWebBox(object):
-    """communication with a 'Sunny WebBox', a monitoring system for solar plants"""
+class SunnyWebBoxBase(object):
+    """Communication with a 'Sunny WebBox', a monitoring system for solar plants.
+       This is an abstract base class, please use SunnyWebBoxHTTP or
+       SunnyWebBoxUDPStream instead!
+    """
     
     def __init__(self, host='1.2.3.4', password=''):
         self.host = host
@@ -41,8 +59,11 @@ class SunnyWebBox(object):
             self.pwenc = hashlib.md5(password).hexdigest()
         else:
             self.pwenc = ''
-        self.conn  = httplib.HTTPConnection(self.host)
-        self.count = Counter()
+        self.count = Counter(1)
+        self.openConnection()
+    
+    def openConnection(self):
+        raise Exception('Abstract base class, use child class instead!')
 
     def newRequest(self, name='GetPlantOverview', withPW=False, **params):
         """return a new rpc request structure (dictionary)"""
@@ -53,28 +74,22 @@ class SunnyWebBox(object):
         if params:
             r['params'] = params
         return r
-            
-    def _rpc(self, request):
-        """send an rpc request as JSON object via http and read the result"""
-        js = json.dumps(request)
-        self.conn.request('POST', '/rpc', "RPC=%s" % js)
-        response = json.loads(self.conn.getresponse().read())
-        if response['id'] != request['id']:
-            raise Exception('RPC answer has wrong id!')
-        return response['result']
-        
+
+    def _rpc(self, *args):
+        raise Exception('Abstract base class, use child class instead!')
+
     def printOverview(self):
         """print a short overview of the plant data"""
         for v in self.getPlantOverview():
             print("%15s (%15s): %s %s" % 
                 (v['name'], v['meta'], v['value'], v['unit']))
-        
+
     # wrapper methods for the RPC functions
-        
+
     def getPlantOverview(self):
         res = self._rpc(self.newRequest('GetPlantOverview'))
         return res['overview']
-        
+
     def getDevices(self):
         res = self._rpc(self.newRequest('GetDevices'))
         return res['devices']
@@ -91,7 +106,7 @@ class SunnyWebBox(object):
         for l in res['devices']:
             r[l['key']] = l['channels']
         return r
-        
+
     def getParameterChannels(self, deviceKey):
         res = self._rpc(self.newRequest('GetParameterChannels', withPW=True, device=deviceKey))
         return res[deviceKey]
@@ -104,30 +119,76 @@ class SunnyWebBox(object):
         for l in res['devices']:
             r[l['key']] = l['channels']
         return r
-        
+
     def setParameter(self, *args):
         raise Exception('Not yet implemented!')
 
 
+class SunnyWebBoxHTTP(SunnyWebBoxBase):
+    """Communication with a 'Sunny WebBox' via HTTP."""
+    
+    def openConnection(self):
+        if py3:
+            from http.client import HTTPConnection
+        else:
+            from httplib import HTTPConnection
+        self.conn  = HTTPConnection(self.host)
+
+    def _rpc(self, request):
+        """send an rpc request as JSON object via http and read the result"""
+        js = json.dumps(request)
+        self.conn.request('POST', '/rpc', "RPC=%s" % js)
+        tmp = buf2str(self.conn.getresponse().read())
+        response = json.loads(tmp)
+        if response['id'] != request['id']:
+            raise Exception('RPC answer has wrong id!')
+        return response['result']
+
+
+class SunnyWebBoxUDPStream(SunnyWebBoxBase):
+    """Communication with a 'Sunny WebBox' via UDP Stream."""
+    
+    def openConnection(self):
+        from socket import socket, AF_INET, SOCK_DGRAM
+        self.udpPort = 34268
+        self.ssock = socket(AF_INET, SOCK_DGRAM)
+        self.rsock = socket(AF_INET, SOCK_DGRAM)
+        self.rsock.bind(("", self.udpPort))
+        self.rsock.settimeout(100.0)
+
+    def _rpc(self, request):
+        """send an rpc request as JSON object via UDP Stream and read the result"""
+        js = ''.join(i+'\0' for i in json.dumps(request, separators=(',',':')))
+        self.ssock.sendto(str2buf(js), (self.host, self.udpPort))
+        while True:
+            data, addr = self.rsock.recvfrom(10*1024)
+            if addr[0] == self.host:
+                break
+        tmp = buf2str(data).replace('\0', '')
+        response = json.loads(tmp)
+        if response['id'] != request['id']:
+            raise Exception('RPC answer has wrong id!')
+        return response['result']
+
+
 if __name__ == '__main__':
     
-    import sys
-
     pw = ''
     ip = sys.argv[1]
     if len(sys.argv) > 2:
         pw = sys.argv[2]
     
-    swb = SunnyWebBox(ip, password=pw)
+    swb = SunnyWebBoxHTTP(ip, password=pw)
+    # swb = SunnyWebBoxUDPStream(ip, password=pw)
 
-    print '###### Overview:'
+    print('###### Overview:')
     swb.printOverview()
     
     for d in swb.getDevices():
         devKey = d['key']
-        print "\n  #### Device %s (%s):" % (devKey, d['name'])
+        print("\n  #### Device %s (%s):" % (devKey, d['name']))
         
-        print "\n    ## Process data:"
+        print("\n    ## Process data:")
         channels = swb.getProcessDataChannels(devKey)
         data = swb.getProcessData([{'key':devKey, 'channels':channels}])
         for c in data[devKey]:
@@ -135,7 +196,7 @@ if __name__ == '__main__':
                (c['name'], c['meta'], c['value'], c['unit']))
 
                
-        print "\n    ## Parameters:"
+        print("\n    ## Parameters:")
         channels = swb.getParameterChannels(devKey)
         data = swb.getParameter([{'key':devKey, 'channels':channels}])
         for c in data[devKey]:
